c609501d1a3eaaf72e50fad1e79878266a02f5e6
Derek <derek.stefanik@icloud.com>
2020-06-12 21:52:43 -0400

    swapped out PATH code in BLASTapp.r

--- BLASTapp.r
+++ BLASTapp.r
@@ -1,7 +1,8 @@
 #!/usr/bin/env Rscript
 
-library(withr)
-with_path("/usr/local/ncbi/blast/bin/", Sys.getenv("PATH"), "prefix")
+#set path for BLAST executables 
+old_path <- Sys.getenv("PATH")
+Sys.setenv(PATH = paste(old_path, "/home/ubuntu/ncbi-blast-2.10.1+/bin/", sep = ":"))
 
 ###################### 
 # this is the old way to run it locally
