26bf252009ee1ec47579d897f2113166a26de607
Derek <derek.stefanik@icloud.com>
2020-06-17 13:12:29 -0400

    Changed BLASTapp.r directory

--- BLASTapp.r
+++ BLASTapp.r
@@ -2,8 +2,8 @@
 
 #set path for BLAST executables 
 old_path <- Sys.getenv("PATH")
-Sys.setenv(PATH = paste(old_path, "/home/ubuntu/ncbi-blast-2.10.1+/bin/", sep = ":"))
+Sys.setenv(PATH = paste(old_path, "~/ncbi-blast-2.10.1+/bin/", sep = ":"))
 
 ###################### 
 # this is the old way to run it locally
-shiny::runApp("/home/ubuntu/ShinyWebApp/")
+shiny::runApp("~/ShinyWebApp/")
